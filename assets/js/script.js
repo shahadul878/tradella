0bcb6ec544a48bb2b303101d368bf1e13c77461a
H M Shahadul Islam <shahadul.islam1@gmail.com>
2025-05-17 04:01:12 +0600

    update

---
+++ assets/js/script.js
@@ -0,0 +1,10 @@
+// Custom JS for Tradella International
+$(document).ready(function(){
+    $('.hero-slider').slick({
+        items:1,
+        loop:true,
+        autoplay:true,
+        arrows:false,
+        dots:true
+    });
+});
